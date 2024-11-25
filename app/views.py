ecb0e0c8bae6c2942c366a3847adc5e9a4178e9d
haneefb7 <ymdhaneef7@gmail.com>
2024-11-25 12:45:40 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def haneef(request):
+    return HttpResponse('This is Haneef i am going to create a app ')
